2ad6f0e047563c1812190b206a79aa61b808c6ff
adee11233 <aleksnik3012@gmail.com>
2024-03-26 18:33:11 +0200

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function App() {
+  return (
+    <div className="border-2 w-screen h-screen">
+      <p className="text-4xl">Sticky Wall</p>
+      <div className="">connent</div>
+    </div>
+  );
+}
+
+export default App;
