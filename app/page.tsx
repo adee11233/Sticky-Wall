406e5c2ef64472c2bf123599128732d7adc796ef
adee11233 <aleksnik3012@gmail.com>
2024-03-26 20:43:10 +0200

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { Plus } from "lucide-react";
+
 function App() {
   return (
-    <div className="border-2 w-screen h-screen">
-      <p className="text-4xl">Sticky Wall</p>
-      <div className="">connent</div>
+    <div className="border-2 w-screen h-screen p-5">
+      <p className="text-4xl font-bold">Sticky Wall</p>
+      <div className=" border-2 w-full h-full ">
+        connent
+        <button className="border-2 w-60 h-60 flex items-center justify-center">
+          <Plus className="w-1/3 h-1/3 " />
+        </button>
+      </div>
     </div>
   );
 }
